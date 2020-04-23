d14e306467a10d07bff583e64fbc5fa52cb63fea
Charlie Maloney <charliemaloney200@gmail.com>
2020-04-23 10:00:23 -0700

    added some comments to the routes

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -2,14 +2,13 @@ const express = require('express');
 const apiController = require('../controllers/apiController.js');
 const router = express.Router();
 
-// connects with SQL database to serve client/front-end
-// res is array of objects with name, parkCode, lat, and long
 
+// Retrieves all parks from the DB on initial page load
 router.get('/getparks', apiController.getParkData, (req, res) => {
   res.status(200).json(res.locals.parksData);
 });
 
-// onclick on client side request for single park info
+// Get more information on a specific park
 router.get('/getparks/:code', apiController.getOnePark, (req, res) => {
   res.status(200).json(res.locals.parkData);
 });
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,14 +2,17 @@ const express = require('express');
 const userController = require('../controllers/userController.js');
 const router = express.Router();
 
+// Creates user account, hashing password with Bcrypt
 router.post('/signup', userController.signUp, userController.getData, (req, res) => {
   res.status(200).json(res.locals.user)
 });
 
+// Verifies user at login
 router.post('/login', userController.login, userController.getData, (req, res) =>
 res.status(200).json(res.locals.user)
 );
 
+// Adds a favorite park to the user's profile
 router.post('/favorite', userController.updateFavorites, userController.getData, (req, res) => {
   res.status(200).json(res.locals.user)
 } )
